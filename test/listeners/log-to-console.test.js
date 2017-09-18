23c117d12755f13c47c381ab68dbd5cdd01ecb42
Ian Savchenko (isao) <ian.savchenko@tobii.com>
2017-09-18 15:00:15 +0200

    Added tests

---
+++ test/listeners/log-to-console.test.js
@@ -0,0 +1,34 @@
+import MinLog from '../../src/minlog';
+import _ from 'lodash';
+
+import {
+  _levelToConsoleFun
+} from '../../src/listeners/log-to-console';
+
+describe('logToConsole listener', function() {
+  describe('_levelToConsoleFun', function() {
+    it('should not regress (default levels)', function() {
+      let instance = new MinLog();
+      let snapshot = {};
+
+      _.forEach(instance.levels, function(level, levelName) {
+        let levels = [
+          level - 1,
+          level,
+          level + 1,
+          levelName,
+          `x${levelName}x`
+        ];
+
+        _.forEach(levels, function(level) {
+          snapshot[level] = _levelToConsoleFun({
+            level,
+            levels: instance.levels
+          });
+        });
+      });
+
+      expect(snapshot).toMatchSnapshot();
+    });
+  });
+});
