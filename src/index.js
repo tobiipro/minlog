be336f3f08367f02f460012548adfa9af245c9c5
Andrei Neculau <andrei.neculau@gmail.com>
2019-02-23 23:47:51 +0100

    make it easier to expose default levels

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import MinLog from './minlog';
 import logToConsole from './listeners/log-to-console';
 import logToConsoleAwsLambda from './listeners/log-to-console-aws-lambda';
 import serializeErr from './serializers/err';
 import serializeTime from './serializers/time';
 
+import {
+  MinLog,
+  defaultLevels
+} from './minlog';
+
 export {
   MinLog,
+  defaultLevels,
   logToConsole,
   logToConsoleAwsLambda,
   serializeErr,
--- src/minlog.js
+++ src/minlog.js
@@ -4,30 +4,32 @@ import {
   getCallerInfo
 } from './util';
 
-export default class MinLog {
-  levels = {
-    time: 70,
+export let defaultLevels = {
+  time: 70,
 
-    // npm alias
-    fatal: 0, // emergency
-    verbose: 70, // debug
-    silly: 80,
+  // npm alias
+  fatal: 0, // emergency
+  verbose: 70, // debug
+  silly: 80,
 
-    // https://tools.ietf.org/html/rfc3164 (multiplier 10)
-    emergency: 0,
-    alert: 10,
-    critical: 20,
-    error: 30,
-    warning: 40,
-    notice: 50,
-    informational: 60,
-    debug: 70,
+  // https://tools.ietf.org/html/rfc3164 (multiplier 10)
+  emergency: 0,
+  alert: 10,
+  critical: 20,
+  error: 30,
+  warning: 40,
+  notice: 50,
+  informational: 60,
+  debug: 70,
 
-    // console
-    warn: 40, // warning
-    info: 60, // informational
-    trace: 90
-  };
+  // console
+  warn: 40, // warning
+  info: 60, // informational
+  trace: 90
+};
+
+export class MinLog {
+  levels = defaultLevels;
 
   serializers = [];
 
@@ -153,3 +155,5 @@ export default class MinLog {
     this.time(label, entry);
   }
 }
+
+export default MinLog;
