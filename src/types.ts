32500d821fb6db3c15d39347955204404c5aca33
Andrei Neculau <andrei.neculau@gmail.com>
2019-11-14 21:07:58 +0100

    use types from lodash-firecloud

--- src/types.ts
+++ src/types.ts
@@ -1,51 +1,6 @@
-export type MaybePromise<T> =
-  T | Promise<T>;
-
-/* Functions */
-
-export type Fn<
-  TReturn = unknown,
-  TArgs extends unknown[] = unknown[]
-> =
-  (...args: TArgs) => TReturn;
-
-export type AsyncFn<
-  TReturn = unknown,
-  TArgs extends unknown[] = unknown[]
-> =
-  (...args: TArgs) => Promise<TReturn>;
-
-export type Unpromise<TMaybePromise extends any> =
-  TMaybePromise extends Promise<infer TValue>
-    ? TValue
-    : TMaybePromise
-
-export type MaybePromiseReturn<
-  TReturn,
-  T extends Fn
-> =
-  ReturnType<T> extends Promise<any> ? Promise<TReturn> : TReturn;
-
-export type FnChangeReturnType<
-  T extends Fn,
-  TReturn = Unpromise<ReturnType<T>>
-> =
-  ReturnType<T> extends Promise<any> ?
-    (...args: Parameters<T>) => Promise<TReturn> :
-    (...args: Parameters<T>) => TReturn;
-
-/* Callbacks */
-
-export type CallbackFn =
-  Fn<MaybePromise<void>>;
-
-export type NodeCallbackFn<TResult = unknown> =
-  Fn<MaybePromise<void>, [Error, TResult]>;
-
-export type NodeCallbackErrorLastFn<TResult = unknown> =
-  Fn<MaybePromise<void>, [TResult, Error]>;
-
-/* MinLog */
+import {
+  MaybePromise
+} from 'lodash-firecloud/types';
 
 export type MinLogLevelName = string;
 
