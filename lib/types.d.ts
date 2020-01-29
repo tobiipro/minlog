ab49c0f4f53e719adc5429c681f57e2101bf1458
Andrei Neculau <andrei.neculau@gmail.com>
2020-01-30 00:49:40 +0100

    0.8.5 (dist)

---
+++ lib/types.d.ts
@@ -0,0 +1,67 @@
+import { MaybePromise } from 'lodash-firecloud/types';
+export declare type MinLog = import('./minlog').MinLog;
+export declare type MinLogLevelName = string;
+export declare type MinLogLevelCode = number;
+export declare type MinLogLevel = MinLogLevelName | MinLogLevelCode;
+export declare type MinLogLevelNameToCode = {
+    [key: string]: MinLogLevelCode;
+};
+export declare type MinLogOptions = {
+    serializers?: MinLogSerializer[];
+    listeners?: MinLogListener[];
+    levels?: MinLogLevelNameToCode;
+    requireRawEntry?: boolean;
+    requireSrc?: boolean;
+};
+export interface MinLogError extends Error {
+    uncaught?: boolean;
+    inPromise?: boolean;
+}
+export interface MinLogRawEntry {
+    _args: any[];
+    _time: number;
+    _level: MinLogLevelCode;
+    _src?: {
+        file: string;
+        line: number;
+        function: string;
+    };
+    _babelSrc?: {
+        file: string;
+        line: number;
+        column: number;
+        function: string;
+    };
+    msg?: string;
+    err?: MinLogError;
+}
+export interface MinLogSerializedTime {
+    stamp: string;
+    localStamp: string;
+    zone: string;
+    utc_offset: number;
+}
+export interface MinLogSerializedError extends Omit<MinLogError, 'stack'> {
+    stack: string[];
+}
+export interface MinLogEntry extends Omit<MinLogRawEntry, '_time' | 'err'> {
+    _time: MinLogRawEntry['_time'] | MinLogSerializedTime;
+    err?: MinLogError | MinLogSerializedError;
+    _duration?: {
+        stamp: string;
+        human: string;
+        ms: number;
+    };
+}
+export declare type MinLogListener = (args: {
+    entry: MinLogEntry;
+    logger: import('./minlog').MinLog;
+    rawEntry?: MinLogRawEntry;
+}) => MaybePromise<void>;
+export declare type MinLogSerializer = (args: {
+    entry: MinLogEntry;
+    logger: import('./minlog').MinLog;
+    rawEntry?: MinLogRawEntry;
+}) => MaybePromise<MinLogEntry>;
+export declare type MinLogFormatArgs = [string, ...any[]];
+export declare type MinLogArg = string | Error | object;
